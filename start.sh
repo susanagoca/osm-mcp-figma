26033ecc0a733997d0f8c908771007c36fd71fdf
susanagoca <susanagocano@gmail.com>
2026-02-23 22:35:10 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,7 @@ set -e
 
 PORT_TO_USE="${PORT:-8080}"
 
-mcp-proxy --host=0.0.0.0 --port="$PORT_TO_USE" --allow-origin "*" uvx osm-mcp-server
+python -m pip install --upgrade pip
+python -m pip install uv
+
+mcp-proxy --host=0.0.0.0 --port="$PORT_TO_USE" --allow-origin "*" uvx --from git+https://github.com/jagan-shanmugam/open-streetmap-mcp.git osm-mcp-server
