03491bc79e898f13a577fee2a6900c1b1e0df7c0
susanagoca <susanagocano@gmail.com>
2026-02-23 23:30:40 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,3 +5,4 @@ PORT_TO_USE="${PORT:-8080}"
 
 mcp-proxy --host 0.0.0.0 --port "$PORT_TO_USE" --allow-origin "*" -- \
   uvx --from git+https://github.com/jagan-shanmugam/open-streetmap-mcp.git osm-mcp-server
+  
